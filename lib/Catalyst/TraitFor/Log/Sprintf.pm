7c27c55981f060d3d56e74ca11d6f83de3e51b6b
Michael Jemmeson <michael.jemmeson@foxtons.co.uk>
2012-08-10 16:34:16 +0100

    added flatten feature (from CatalystX::Controller::Sugar)

--- lib/Catalyst/TraitFor/Log/Sprintf.pm
+++ lib/Catalyst/TraitFor/Log/Sprintf.pm
@@ -3,8 +3,13 @@ package Catalyst::TraitFor::Log::Sprintf;
 use 5.008001;
 
 use Moose::Role;
+use Data::Dumper ();
 
 {
+    local $Data::Dumper::Indent   = 0;
+    local $Data::Dumper::Maxdepth = $Data::Dumper::Maxdepth || 4;
+    local $Data::Dumper::Terse    = 0;
+
     my $meta = Class::MOP::get_metaclass_by_name(__PACKAGE__);
 
     while ( my ( $name, $level ) = each %Catalyst::Log::LEVELS ) {
@@ -15,13 +20,20 @@ use Moose::Role;
                 my ( $self, $format, @args ) = @_;
 
                 if ( $self->level & $level ) {
-                    $self->_log(
-                        $name,
-                        @args ? sprintf( $format, @args ) : $format    #
-                    );
+                    $self->_log( $name, sprintf( $format, _flatten(@args) ) );
                 }
             }
         );
+    }
+
+    # borrowed from CatalystX::Controller::Sugar
+
+    sub _flatten {
+        map {
+                  ref $_     ? Data::Dumper::Dumper($_)
+                : defined $_ ? $_
+                : '__UNDEF__'
+        } @_;
     }
 }
 
